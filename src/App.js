c2bba8b49bf4d0c2a9e865b004b247f76bd5fdf4
설하 <seolha0@protonmail.ch>
2022-10-05 17:02:05 +0900

    #5 REFCT; <Subject> 클래스 리팩토링

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class Subject extends Component{
   render(){
     return (
       <header>
-        <h1>REACT</h1>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.sub}</h2>
       </header>
     )
   }
@@ -16,7 +17,7 @@ class Contents extends Component {
   render(){
     return(
       <article>
-        <h2>HELP</h2>
+        <h3>HELP</h3>
         I don't wanna be a slave
       </article>
     )
@@ -27,7 +28,7 @@ class App extends Component{
   render(){
     return (
       <div className="App">
-        <Subject></Subject>
+        <Subject title='Web' sub='React'></Subject>
         <Contents></Contents>
         blyat
       </div>
