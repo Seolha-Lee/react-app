ed7e8a321feef8920d04b0a4e4b0db5623fcedb3
설하 <seolha0@protonmail.ch>
2022-10-05 20:18:52 +0900

    #6 REFCT; 편집 구역 일원화

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
-
-class Subject extends Component{
-  render(){
-    return (
-      <header>
-        <h1>{this.props.title}</h1>
-        <h2>{this.props.sub}</h2>
-      </header>
-    )
-  }
-}
-
-class Contents extends Component {
-  render(){
-    return(
-      <article>
-        <h3>HELP</h3>
-        I don't wanna be a slave
-      </article>
-    )
-  }
-}
+import Subject from './components/Subject';
+import Nav from './components/Nav';
+import Contents from './components/Contents';
 
 class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      mode:'welcome',
+      subject:{title:'WEB', sub:'React'},
+      welcome:{title:'Welcome', desc:'Hello, React!'},
+      contents:[{id:1, title:'HTML', desc:'HTML can be refered to bones.'},
+      {id:2, title:'CSS', desc:'CSS can be refered to skins.'},
+      {id:3, title:'JS', desc:'JavaScript can be refered to organs.'}]
+    }
+  }
   render(){
+    console.log('App render')
+    var _title, _desc = null
+  if (this.state.mode === 'welcome'){
+    _title = this.state.welcome.title
+    _desc = this.state.welcome.desc
+  } else if (this.state.mode === 'read'){
+    _title = this.state.contents[0].title
+    _desc = this.state.contents[0].desc
+  }
     return (
       <div className="App">
-        <Subject title='Web' sub='React'></Subject>
-        <Contents></Contents>
-        blyat
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        <Nav cont={this.state.contents}></Nav>
+        <Contents title={_title} desc={_desc}></Contents>
       </div>
     )
   }
