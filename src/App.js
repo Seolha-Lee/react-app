8565793d7581ac903ab0f94a2805c489fe4039bb
설하 <seolha0@protonmail.ch>
2022-11-23 20:19:05 +0900

    #8 UPDATE; 개시물 CREATE 기능 추가, UPDATE 추가 예정

--- src/App.js
+++ src/App.js
@@ -2,40 +2,83 @@ import './App.css';
 import React, { Component } from 'react';
 import Subject from './components/Subject';
 import Nav from './components/Nav';
-import Contents from './components/Contents';
+import Create from './components/Create';
+import DataManager from './components/DataManager';
+import Read from './components/Read';
 
 class App extends Component{
   constructor(props){
     super(props)
-    this.state = {
-      mode:'welcome',
-      subject:{title:'WEB', sub:'React'},
-      welcome:{title:'Welcome', desc:'Hello, React!'},
-      contents:[
-      {id:1, title:'HTML', desc:'HTML can be refered to bones.'},
-      {id:2, title:'CSS', desc:'CSS can be refered to skins.'},
-      {id:3, title:'JS', desc:'JavaScript can be refered to organs.'}]
+    this.id_max = 3;
+    this.state = { /* Rendered */
+      mode:`create`,
+      cont_id_sel: 1,
+      subject:{title:`WEB`, sub:`React`},
+      welcome:{title:`Welcome`, contents:`Welcome page`},
+      posts:[
+      {id:1, title:`1st`, contents:`Contents of post id:1`},
+      {id:2, title:`2nd`, contents:`Contents of post id:2`},
+      {id:3, title:`3rd`, contents:`Contents of post id:3`}]
     }
   }
   render(){
-    console.log('App render')
-    var _title, _desc = null
-  if (this.state.mode === 'welcome'){
-    _title = this.state.welcome.title
-    _desc = this.state.welcome.desc
-  } else if (this.state.mode === 'read'){
-    _title = this.state.contents[0].title
-    _desc = this.state.contents[0].desc
-  }
+    /* No-reload page reaction */
+    console.log(`App render`)
+    var _title, _contents, _article = null
+    if (this.state.mode === `welcome`){
+      _title = this.state.welcome.title
+      _contents = this.state.welcome.contents
+      _article = <Read title={_title} contents={_contents}></Read>
+    } else if (this.state.mode === `read`){
+      let i = 0
+      while (i < this.state.posts.length){
+        let data = this.state.posts[i]
+        if(data.id === this.state.cont_id_sel){
+          _title = data.title
+          _contents = data.contents
+          break
+        }
+        i += 1
+      }
+      _article = <Read title={_title} contents={_contents}></Read>
+    } else if (this.state.mode === `create`){
+      _article = <Create onSubmit={
+        function(_title, _contents){
+          this.id_max += 1
+          let _posts = this.state.posts.concat(
+            {id: this.id_max, title: _title, contents: _contents}
+          )
+          this.setState({posts: _posts})
+          console.log(_title, _contents)
+        }.bind(this)
+      }></Create>
+    }
+    /*  */
+
     return (
       <div className="App">
         <Subject title={this.state.subject.title} sub={this.state.subject.sub}
         onChangePage={function(){
-          this.setState({mode:'welcome'});
+          this.setState({mode:`welcome`});
         }.bind(this)}
         ></Subject>
-        <Nav cont={this.state.contents}></Nav>
-        <Contents title={_title} desc={_desc}></Contents>
+        <Nav onChangePage={
+          function(id){
+            this.setState({
+              mode:`read`,
+              cont_id_sel: Number(id)
+            })
+          }.bind(this)
+        }
+          cont={this.state.posts}></Nav>
+        
+        <DataManager onChangeMode={function(_mode){
+          this.setState({
+            mode: _mode
+          })
+        }.bind(this)}></DataManager>
+
+        {_article}
       </div>
     )
   }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,32 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
 
 class Nav extends Component {
+    shouldComponentUpdate(updatedprops, updatedstate){
+        console.log('Navigation render(shouldComponentUpdate)'
+        , updatedprops.data
+        , this.props.data
+        )
+        if (this.props.data !== updatedprops.data){
+            return true
+        }
+        return false
+    }
     render(){
         console.log('Navigation render')
         var lists = []
         var data = this.props.cont
         var i = 0
         while(i < data.length){
-            lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>)
-            i += 1;
+            lists.push(<li key={data[i].id}>
+                <a href={"/posts/"+data[i].id}
+                    data-id={data[i].id}
+                    onClick={function(e){
+                    e.preventDefault();
+                    this.props.onChangePage(e.target.dataset.id)
+
+                }.bind(this)}>{data[i].title}</a>
+            </li>)
+            i += 1
         }
         return(
             <article>
