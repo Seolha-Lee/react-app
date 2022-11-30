b75b11be65fecec1f0180d7f0809cb69cadea461
설하 <seolha0@protonmail.ch>
2022-11-30 20:23:34 +0900

    #9 UPDATE; Update와 Delete 기능 추가

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import Nav from './components/Nav';
 import Create from './components/Create';
 import DataManager from './components/DataManager';
 import Read from './components/Read';
+import Update from './components/Update';
 
 class App extends Component{
   constructor(props){
     super(props)
     this.id_max = 3;
     this.state = { /* Rendered */
-      mode:`create`,
+      mode:`welcome`,
       cont_id_sel: 1,
       subject:{title:`WEB`, sub:`React`},
       welcome:{title:`Welcome`, contents:`Welcome page`},
@@ -21,7 +22,17 @@ class App extends Component{
       {id:3, title:`3rd`, contents:`Contents of post id:3`}]
     }
   }
-  render(){
+  readContents(){
+    let i = 0
+    while (i < this.state.posts.length){
+      let data = this.state.posts[i]
+      if(data.id === this.state.cont_id_sel){
+        return data
+      }
+      i += 1
+    }
+  }
+  getContents(){
     /* No-reload page reaction */
     console.log(`App render`)
     var _title, _contents, _article = null
@@ -29,22 +40,17 @@ class App extends Component{
       _title = this.state.welcome.title
       _contents = this.state.welcome.contents
       _article = <Read title={_title} contents={_contents}></Read>
-    } else if (this.state.mode === `read`){
-      let i = 0
-      while (i < this.state.posts.length){
-        let data = this.state.posts[i]
-        if(data.id === this.state.cont_id_sel){
-          _title = data.title
-          _contents = data.contents
-          break
-        }
-        i += 1
-      }
-      _article = <Read title={_title} contents={_contents}></Read>
-    } else if (this.state.mode === `create`){
+    }
+    
+    else if (this.state.mode === `read`){
+      _contents = this.readContents()
+      _article = <Read title={_contents.title} contents={_contents.contents}></Read>
+    }
+    
+    else if (this.state.mode === `create`){
       _article = <Create onSubmit={
         function(_title, _contents){
-          this.id_max += 1
+          this.id_max++
           let _posts = this.state.posts.concat(
             {id: this.id_max, title: _title, contents: _contents}
           )
@@ -53,8 +59,28 @@ class App extends Component{
         }.bind(this)
       }></Create>
     }
-    /*  */
-
+    
+    else if (this.state.mode === `update`){
+      _contents = this.readContents()
+      _article = <Update data={_contents} onSubmit={
+        function(_id, _title, _contents){
+          let _posts = Array.from(this.state.posts)
+          let i = 0
+          while(i < _posts.length){
+            if(_posts[i].id === _id) {
+              _posts[i] = {id: _id, title: _title, contents: _contents}
+              break
+            }
+            i++
+          }
+          this.setState({posts: _posts, mode: `read`})
+          console.log(_title, _contents)
+        }.bind(this)
+      }></Update>
+    }
+    return _article
+  }
+  render(){
     return (
       <div className="App">
         <Subject title={this.state.subject.title} sub={this.state.subject.sub}
@@ -73,12 +99,28 @@ class App extends Component{
           cont={this.state.posts}></Nav>
         
         <DataManager onChangeMode={function(_mode){
+          if(_mode === 'delete'){
+            if(window.confirm()){
+              let _posts = Array.from(this.state.posts)
+              let i = 0
+              while(i < _posts.length){
+                if(_posts[i].id === this.state.cont_id_sel){
+                  _posts.splice(i, 1)
+                  break
+                }
+                i++
+              }
+              this.setState({mode: 'welcome', posts: _posts})
+            }
+          } else {
+
+          }
           this.setState({
             mode: _mode
           })
         }.bind(this)}></DataManager>
 
-        {_article}
+        {this.getContents()}
       </div>
     )
   }
